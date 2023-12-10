81f1f4f746cf13ff21fddf52fc8f70df17dfee14
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-12-09 20:30:47 -0600

    Inicio de proyecto

---
+++ index.js
@@ -0,0 +1,12 @@
+var ctx;
+var canvas;
+var palabra;
+var letras = "QWERTYUIOPASDFGHJKLÑZXCVBNM";
+var colorTeclas = "black"
+var colorMargen = "lightgray";
+var inicioX = 200;
+var inicioY = 300;
+var lon = 35;
+var margen = 20;
+var teclas_array = new Array();
+var palabra_array = new Array();
