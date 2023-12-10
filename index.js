2212647eaff3c6b56fafc1881a215138786ec9f7
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-12-09 20:38:19 -0600

    Palabras para juego

--- index.js
+++ index.js
@@ -10,3 +10,58 @@ var lon = 35;
 var margen = 20;
 var teclas_array = new Array();
 var palabra_array = new Array();
+var aciertos = 0;
+var errores = 0;
+
+//palabras para juego
+
+palabras_array.push("LEÓN");
+palabras_array.push("CABALLO");
+palabras_array.push("PERRO");
+palabras_array.push("GATO");
+palabras_array.push("LAGARTIJA");
+palabras_array.push("RINOCERONTE");
+palabras_array.push("TIBURÓN");
+palabras_array.push("CARACOL");
+palabras_array.push("ALACRÁN");
+palabras_array.push("ARAÑA");
+palabras_array.push("CHAPULIN");
+palabras_array.push("AVESTRUZ");
+palabras_array.push("OCELOTE");
+palabras_array.push("CANGURO");
+palabras_array.push("ÁGUILA");
+palabras_array.push("MUERCIÉLAGO");
+
+function Tecla(x, y, ancho, alto, letra){
+    this.x = x;
+    this.y = y;
+    this.ancho = ancho;
+    this.alto = alto;
+    this.letra = letra;
+    this.dibuja = dibujaTecla;
+}
+
+function Letra(x, y, ancho, alto, letra){
+    this.x = x;
+    this.y = y;
+    this.ancho = ancho;
+    this.alto = alto;
+    this.letra = letra;
+    this.dibuja = dibujaCajaLetra;
+    this.dibujaLetra = dibujaLetraLetra;
+}
+
+window.onload = function() {
+    canvas = document.getElementById("screen");
+    if (canvas && canvas.getContext){
+        ctx = canvas.getContext("2d");
+        if(ctx){
+            teclado();
+            pintaPalabra();
+            horca(errores);
+            canvas.addEventListener("click", selecciona, false);
+        } else {
+            alert ("Error al cargar el contexto!");
+        }
+    }
+}
