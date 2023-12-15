17d29d704a147640db2e3a5bef4c12d056f8b8ef
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-12-14 23:20:03 -0600

    Funciones coordenadas y comparar

--- index.js
+++ index.js
@@ -77,6 +77,134 @@ function dibujaCajaLetra(){
     ctx.strokeRect(this.x, this.y, this.ancho, this.alto);
 }
 
+function pistaFunction(palabra){
+    let pista = "";
+    switch(palabra){
+        case 'LEON':
+            pista = "Ruge y es fuerte.";
+            break; 
+        case 'CABALLO':
+            pista = "Hay de tierra y hay de mar.";
+            break;
+        case 'PERRO':
+            pista = "El mejor amigo del hombre.";
+            break;
+        case 'GATO':
+            pista = "Son tiernos pero arañan.";
+            break;
+        case 'MUERCIÉLAGO':
+            pista = "Se pasan el día refugiados y al caer la tarde salen.";
+        default:
+            pista="No hay pista aun 😶";
+    }
+    
+    ctx.fillStyle = "black";
+    ctx.font = "bold 20px Courier";
+    ctx.fillText(pista, 10, 15);
+}
+
+        
+
+function teclado(){
+    var ren = 0;
+    var col = 0;
+    var letra = "";
+    var miLetra;
+    var x = inicioX;
+    var y = inicioY;
+    for(var i = 0; i < letras.length; i++){
+        letra = letras.substr(i,1);
+        miLetra = new Tecla(x, y, lon, lon, letra);
+        miLetra.dibuja();
+        teclas_array.push(miLetra);
+        x += lon + margen;
+        col++;
+        if(col==10){
+            col = 0;
+            ren++;
+            if(ren==2){
+                x = 280;
+            } else {
+                x = inicioX;
+            }
+        }
+        y = inicioY + ren * 50;
+    }
+}
+
+function pintaPalabra(){
+    var p = Math.floor(Math.random()*palabras_array.length);
+    palabra = palabras_array[p];
+
+    pistaFunction(palabra);
+
+    var w = canvas.width;
+    var len = palabra.length;
+    var ren = 0;
+    var col = 0;
+    var y = 230;
+    var lon = 50;
+    var x = (w - (lon+margen) *len)/2;
+    for(var i=0; i<palabra.length; i++){
+        letra = palabra.substr(i,1);
+        miLetra = new Letra(x, y, lon, lon, letra);
+        miLetra.dibuja();
+        letras_array.push(miLetra);
+        x += lon + margen;
+    }
+}
+
+function horca(errores){
+    var imagen = new Image();
+    imagen.src = "imagenes/ahorcado"+errores+".png";
+    imagen.onload = function(){
+        ctx.drawImage(imagen, 390, 0, 230, 230);
+    }
+    
+    function ajusta(xx, yy){
+        var posCanvas = canvas.getBoundingClientRect();
+        var x = xx-posCanvas.left;
+        var y = yy-posCanvas.top;
+        return{x:x, y:y}
+    }
+    
+    function selecciona(e){
+        var pos = ajusta(e.clientX, e.clientY);
+        var x = pos.x;
+        var y = pos.y;
+        var tecla;
+        var bandera = false;
+        for (var i = 0; i < teclas_array.length; i++){
+            tecla = teclas_array[i];
+            if (tecla.x > 0){
+                if ((x > tecla.x) && (x < tecla.x + tecla.ancho) && (y > tecla.y) && (y < tecla.y + tecla.alto)){
+                    break;
+                }
+            }
+        }
+        if (i < teclas_array.length){
+            for (var i = 0 ; i < palabra.length ; i++){ 
+                letra = palabra.substr(i, 1);
+                if (letra == tecla.letra){
+                    caja = letras_array[i];
+                    caja.dibujaLetra();
+                    aciertos++;
+                    bandera = true;
+                }
+            }
+            if (bandera == false){
+                errores++;
+                horca(errores);
+                if (errores == 5) gameOver(errores);
+            }
+            
+            ctx.clearRect(tecla.x - 1, tecla.y - 1, tecla.ancho + 2, tecla.alto + 2);
+            tecla.x - 1;
+            if (aciertos == palabra.length) gameOver(errores);
+        }
+    }
+}
+
 window.onload = function() {
     canvas = document.getElementById("screen");
     if (canvas && canvas.getContext){
@@ -87,7 +215,7 @@ window.onload = function() {
             horca(errores);
             canvas.addEventListener("click", selecciona, false);
         } else {
-            alert ("Error al cargar el contexto!");
+            alert ("Error al cargar ☹️😬");
         }
     }
 }
